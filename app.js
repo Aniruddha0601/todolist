040c2c1a3ec5de4b57d01b82f7b2540d203124c0
Aniruddha0601 <114240701+Aniruddha0601@users.noreply.github.com>
2023-02-27 22:11:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 const _ = require('lodash');
 
